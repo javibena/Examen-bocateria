5efbeeff133eccc294e1bcb619015510534aa990
Javi <jbenavideseco@gmail.com>
2015-03-13 11:57:06 +0100

    Primer commit

---
+++ Bocateria.java
@@ -0,0 +1,56 @@
+import java.util.HashMap;
+/**
+ * Write a description of class Bocateria here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Bocateria
+{
+    private Cliente PrimeraPersonaEnCola;
+    private int facturacionActual;
+    private HashMap<Integer,Cliente>clientesDespachados;
+    private static int precioBocadillo;
+
+   
+    public Bocateria()
+    {
+        
+    }
+
+   
+    public void llegaNuevoClienteALaCola(int numeroDeBocadillos)
+    {
+        
+    }
+    
+    public void visualizaDatosClientesEnCola()
+    {
+        
+    }
+    
+    public void despacharClienteActual()
+    {
+        
+    }
+    
+    public void visualizaDatosBacateria()
+    {
+        
+    }
+    
+    public int getPosicionPrimerClienteConMasBocadillos()
+    {
+        
+    }
+    
+    public void clienteAbandonaLaCola()
+    {
+        
+    }
+    
+    public ovoid ordenarColaPorNumeroDeBocadillos()
+    {
+        
+    }
+}
---
+++ Cliente.java
@@ -0,0 +1,52 @@
+
+/**
+ * Write a description of class Cliente here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Cliente
+{
+    // instance variables - replace the example below with your own
+    private int numeroCliente;
+    private Cliente siguienteEnLaCola;
+    private int numeroDeBocadillos;
+    private static int numeroClienteActual;
+
+    
+    public Cliente(int numeroDeBocadillos)
+    {
+          
+    }
+
+  
+    public int sampleMethod(int y)
+    {
+        
+    }
+    
+    public Cliente getSigienteEnLaCola()
+    {
+        
+    }
+    
+    public int getNumeroDeBocadillos()
+    {
+        
+    }
+    
+    public int getNumeroCliente()
+    {
+        
+    }
+    
+    public void tooString()
+    {
+        
+    }
+    
+    public Cliente setSiguienteEnLaCola()
+    {
+        
+    }
+}
